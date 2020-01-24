b0de1a7868c6958db834fb6ed5291717ac7da53f
Ajandek Peak <peakaja@gmail.com>
2020-01-24 14:48:13 +0100

    git still doesnt ignored even if i cached it

--- training/training_code/prepare_envs.sh
+++ training/training_code/prepare_envs.sh
@@ -1,2 +1,2 @@
-export DATA_DIR=/Users/de165793/GitHub/MAX-Object-Detector/training/sample_training_data
+export DATA_DIR=/Users/de165793/GitHub/MAX-Object-Detector/training/sample_training_data_airplane
 export RESULT_DIR=/Users/de165793/GitHub/MAX-Object-Detector/training/output
